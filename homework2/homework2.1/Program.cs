8726c29c88ed2967818e9af3c13b27c094bc249c
田济雨 <1132341362@qq.com>
2020-09-20 18:16:04 +0800

    first upload

---
+++ homework2/homework2.1/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+namespace homework2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+        }
+
+        static void Test()
+        {
+            Random rand = new Random();
+
+            double area_ = 0;
+            int x = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                var num = rand.Next(0, 4);
+
+                switch (num)
+                {
+                    case 0:
+                        Circle a = new Circle(rand.Next(1, 10));
+                        if (a.Judge()) area_ += a.Area();
+                        else
+                        {
+                            x = 1;
+                            Console.WriteLine("随机生成的圆形半径错误");
+                        }
+                        break;//0是圆形
+                    case 1:
+                        Triangle b = new Triangle(rand.Next(1, 10), rand.Next(1, 10), rand.Next(1, 10));
+                        if (b.Judge()) area_ += b.Area();
+                        else
+                        {
+                            x = 1;
+                            Console.WriteLine("随机生成的三角形三边长错误");
+                        }
+                        break;//1是三角形
+                    case 2:
+                        Rectangle c = new Rectangle();
+                        c.length = rand.Next(1, 10); c.width = rand.Next(1, 10);
+                        if (c.Judge()) area_ += c.Area();
+                        else
+                        {
+                            x = 1;
+                            Console.WriteLine("随机生成的长方形长宽错误");
+                        }
+                        break;//2是长方形
+                    case 3:
+                        Square d = new Square(rand.Next(1, 10));
+                        if (d.Judge()) area_ += d.Area();
+                        else
+                        {
+                            x = 1;
+                            Console.WriteLine("随机生成的正方形边长错误");
+                        }
+                        break;//3是正方形
+                    default:
+                        Console.WriteLine("随机数错误");
+                        x = 1;
+                        break;
+                }
+
+                if (x == 1)
+                {
+                    Console.WriteLine("随机生成图形时错误");
+                    break;
+                }
+
+            }
+            if (x == 0)
+            {
+                Console.WriteLine("随机生成的十个图形的总面积是" + area_);
+            }
+        }
+    }
+
+    interface Type
+    {
+        double Area();
+        bool Judge();
+    }
+    class Triangle : Type
+    {
+        private double side_1, side_2, side_3;
+
+        public Triangle(double side_1, double side_2, double side_3)
+        {
+            this.side_1 = side_1;
+            this.side_2 = side_2;
+            this.side_3 = side_3;
+        }
+        public double Area()
+        {
+            double p = (side_1 + side_2 + side_3) / 2;
+            return System.Math.Sqrt(p * (p - side_1) * (p - side_2) * (p - side_3));
+        }
+        public bool Judge()
+        {
+            if (side_1 + side_2 > side_3 && side_3 + side_2 > side_1 && side_1 + side_3 > side_2) return true;
+            else return false;
+        }
+    }
+
+    class Rectangle : Type
+    {
+        public double length { set; get; }
+        public double width { set; get; }
+
+
+        public double Area()
+        {
+            return this.length * this.width;
+        }
+        public bool Judge()
+        {
+            if (length > 0 && width > 0 && length >= width) return true;
+            else return false;
+        }
+    }
+    class Circle : Type
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double Area()
+        {
+            return this.radius * this.radius * 3.14;
+        }
+        public bool Judge()
+        {
+            if (radius > 0) return true;
+            else return false;
+        }
+    }
+    class Square : Rectangle
+    {
+        public Square(double SideLength)
+        {
+            base.length = SideLength;
+            base.width = SideLength;
+        }
+        new public bool Judge()
+        {
+            if (length > 0) return true;
+            else return false;
+        }
+    }
+
+}
---
+++ homework2/homework2.2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2._2   //clock
+{
+    public delegate void Tick_Alarm(Clock sender, Time time);
+
+    public class Time
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+        public Time()
+        {
+            this.Hour = 0;
+            this.Minute = 0;
+            this.Second = 0;
+        }
+
+        public Time(int x, int y, int z)
+        {
+
+            this.Hour = x;
+            this.Minute = y;
+            this.Second = z;
+        }
+
+    }
+    public class Clock
+
+    {
+        public Time time = new Time();
+        public event Tick_Alarm Tick;
+        public event Tick_Alarm Alarm;
+        public String setTime = DateTime.Now.ToShortTimeString().ToString();
+        public int setSecond = 10;
+        public int setMinute = 0;
+        public int setHour = 0;
+
+        public void Run()   //闹钟运行
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                this.time.Second += 1;
+
+                if (this.time.Second == 60)
+                {
+                    this.time.Second = 0;
+                    this.time.Minute += 1;
+                    if (this.time.Minute == 60)
+                    {
+                        this.time.Minute = 0;
+                        this.time.Hour += 1;
+                        if (this.time.Hour == 24)
+                        {
+                            this.time.Hour = 0;
+                        }
+                    }
+                }
+                Tick(this, this.time);//tick事件
+                Alarm(this, this.time);//alarm事件
+            }
+        }
+    }
+    public class Person
+    {
+        public Clock clock = new Clock();
+        public Person()
+        {
+            clock.Alarm += alarm;
+            clock.Tick += tick;
+        }
+
+        public void tick(Clock sender, Time time)
+        {
+            Console.WriteLine($"[{sender.time.Hour}:{sender.time.Minute}:{sender.time.Second}]:Tick");
+        }
+        public void alarm(Clock sender, Time time)
+        {
+            if (sender.time.Hour == sender.setHour && sender.time.Minute == sender.setMinute && sender.time.Second == sender.setSecond)
+            {
+                Console.WriteLine("Ring");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.clock.Run();
+        }
+    }
+}
